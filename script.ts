4da76ce9f7294771f67235b523dff5c07097d69a
Ruslan <rus.zakirov.2000@bk.ru>
2023-09-08 11:25:16 +0700

    :)

--- script.ts
+++ script.ts
@@ -4,22 +4,10 @@ interface IPerson {
 
 document.addEventListener("DOMContentLoaded", e => {
 
-    // let timerId : number = setInterval(() => {
-    //     console.log("Hello");
-    // }, 500)
-
-    
-    //     clearInterval(timerId)
-    // })
-
-<<<<<<< HEAD
-    console.log("SomeData");
-    
-=======
     
    
     let refOnFunction : () => void = () => {console.log("Hello world");
     }
->>>>>>> 	I do not know what i am doing.
+
 
 })
